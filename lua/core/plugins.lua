199c8898092d43cd2e77ca5c0d1eb7cea5d6a9c7
Niko <radosz2003@gazeta.pl>
2024-01-14 15:47:23 +0100

    snippets etc

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -22,5 +22,9 @@ require("lazy").setup({
 		'williamboman/mason.nvim',
 		'williamboman/mason-lspconfig.nvim',
 		'neovim/nvim-lspconfig',
+		'hrsh7th/nvim-cmp',
+		'hrsh7th/cmp-nvim-lsp',
+		'L3MON4D3/LuaSnip',
+		'saadparwaiz1/cmp_luasnip',
+		'rafamadriz/friendly-snippets',
 	})
-
